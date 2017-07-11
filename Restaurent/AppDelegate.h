5bce51955fbb0c785aca5bf45e9afb22fe70dd69
SAN_Technologies <SportsIT@SportsIT-MacBook-Air.local>
2017-07-11 19:21:21 +0530

    Initial Commit

---
+++ Restaurent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Restaurent
+//
+//  Created by SAN_Technologies on 11/07/17.
+//  Copyright © 2017 San Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
